9bf3529bd0ee3c4478289ed990ff8d8ca7c3178e
mr.gaurav9554 <mr.gaurav9554@gmail.com>
2024-10-02 21:13:35 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-2xl flex justify-center items-center h-screen">
+      <h1>Paradox | IIT Madras | BS Degree</h1>
+    </div>
+  );
+}
