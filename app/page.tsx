12f77b9c591479813f7c95693552bb653dd0eb9c
mr.gaurav9554 <mr.gaurav9554@gmail.com>
2024-10-05 15:50:24 +0000

    fix home page build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,58 @@
-import Image from "next/image";
+import Header from "@/components/header";
+import Login from "@/components/login";
 
 export default function Home() {
   return (
-    <div className="text-2xl flex justify-center items-center h-screen">
-      <h1>Paradox | IIT Madras | BS Degree</h1>
-    </div>
+    <>
+      <Header />
+      <div className="mt-24"></div>
+      <div className="text-2xl flex justify-center items-center bg-red-700 ">
+        <h1 className="text-3xl font-bold bg-slate-900">
+          Paradox | IIT Madras | BS Degree
+        </h1>
+        <Login />
+      </div>
+      <div className="m-5">
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+        <h1 className="">Paradox | IIT Madras | BS Degree</h1>
+      </div>
+    </>
   );
 }
